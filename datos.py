3b8ead4d5747460fdd97d0ebd8f2ad2998f4ab18
BenKuyen <134543583+BenKuyen@users.noreply.github.com>
2023-05-24 14:34:04 -0400

    Archivo datos.py sin validar.

---
+++ datos.py
@@ -0,0 +1,9 @@
+print("DATOS PERSONALES")
+print("-----------------\n")
+vNom=input("Ingrese su nombre:\n> ")
+vEdad=input("Ingrese su edad:\n> ")
+print("-----------------\n")
+print(f"Su nombre es {vNom}.")
+print(f"Su edad es de {vEdad} años.\n")
+
+print("Finalizando proceso...")
